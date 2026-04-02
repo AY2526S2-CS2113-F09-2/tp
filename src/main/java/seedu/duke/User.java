8d456d98794304005364ae0c6677866b744ddd43
tlimnus <e0960089@u.nus.edu>
2026-04-02 12:27:57 +0800

    Completed User Type and Generation refinement

--- src/main/java/seedu/duke/User.java
+++ src/main/java/seedu/duke/User.java
@@ -1,15 +1,50 @@
 package seedu.duke;
 
-public class User {
-    private String name;
-    private Integer number ;
-    private String email;
+import java.util.*;
 
-    public User(String name, Integer number, String email){
+public class User {
+    private static User instance;
+    private String name;
+    private int number;
+    private String email;
+    private static final Ui ui = new Ui();
+
+    private User(String name, int number, String email) {
         this.name = name;
         this.number = number;
         this.email = email;
     }
+
+    public static void userInit() {
+        Scanner sc = new Scanner(System.in);
+        ui.showMessage("Hello, what is your name?");
+        String name = sc.nextLine();
+        ui.showMessage("Hello what is your number?");
+        int number = 0;
+        try {
+            number = sc.nextInt();
+            sc.nextLine();
+        } catch (InputMismatchException e){
+            ui.showMessage("Please enter a number");
+        }
+        ui.showMessage("Finally, what is your email?");
+        String email = sc.nextLine();
+
+        instance = new User(name, number, email);
+    }
+
+    public static void loadFrom(String name, int number, String email) {
+        instance = new User(name, number, email);
+    }
+
+    public static User getInstance() {
+        if (instance == null) {
+            ui.showMessage("You have not been registered.");
+            userInit();
+        }
+        return instance;
+    }
+
 
     public String getName(){
         return this.name;
